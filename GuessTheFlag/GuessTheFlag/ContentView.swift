b74f0b9e59c52f4b9b81e0102a5b73d5dbe695a5
Fromugh <96973078+Fromugh@users.noreply.github.com>
2026-01-14 20:43:18 +0900

    GuessTheFlag
    Chapter 2: GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by H. Sungjae on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.red
+            Text("Your content")
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}

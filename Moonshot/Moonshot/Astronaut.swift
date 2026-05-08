3e1809483fb41496fb2dc4df43e351d59f1e468e
Fromugh <96973078+Fromugh@users.noreply.github.com>
2026-05-08 13:40:13 +0900

    Moonshot

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by H. Sungjae on 3/27/26.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
